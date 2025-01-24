d6bccede237840483f3ec360b0046e25aea320cf
Pojemnik <andrzej.gauza@outlook.com>
2025-01-25 00:45:07 +0100

    Add not working xml parsing

--- Assets/Scripts/Director.cs
+++ Assets/Scripts/Director.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.IO;
 using System.Xml;
@@ -6,25 +7,69 @@ using UnityEngine.Networking;
 
 class Director : Singleton<Director>
 {
+    private string documentText = null;
     private XmlDocument currentDocument = new XmlDocument();
+
+    public bool ScriptLoaded => documentText != null;
 
     public IEnumerator LoadScript(Script script)
     {
         string path = script.Path;
-#if UNITY_WEBGL
+#if (UNITY_WEBGL && !UNITY_EDITOR)
         using (UnityWebRequest request = UnityWebRequest.Get(path))
         {
             yield return request.SendWebRequest();
             Debug.Log($"Result of request to {script.Path}: {request.result}");
-            string text = request.downloadHandler.text;
-            Debug.Log($"Read data begining: {text.Substring(0, 100)}");
-            currentDocument.LoadXml(text);
+            documentText = request.downloadHandler.text;
         }
 #else
-            StreamReader reader = new StreamReader(path);
-            string text = reader.ReadToEnd();
-            Debug.Log($"Read data begining: {text.Substring(0, 100)}");
-            currentDocument.LoadXml(text);
+        StreamReader reader = new StreamReader(path);
+        documentText = reader.ReadToEnd();
+        yield return null;
 #endif
+    }
+
+    public void ReadScript()
+    {
+        Debug.Log($"Read data begining: {documentText.Substring(0, 100)}");
+        try
+        {
+            currentDocument.LoadXml(documentText);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Read XML exception: {e}");
+        }
+    }
+
+    public void RunScript()
+    {
+        Debug.Log("Run script");
+        XmlNode node = currentDocument.FirstChild;
+        do
+        {
+            node = ProcessNode(node);
+        }
+        while (node != currentDocument);
+    }
+
+    private XmlNode ProcessNode(XmlNode node)
+    {
+        Debug.Log(node.Name);
+        return GetNextNode(node);
+    }
+
+    private XmlNode GetNextNode(XmlNode node)
+    {
+        if (node.HasChildNodes)
+        {
+            return node.FirstChild;
+        }
+        XmlNode sibling = node.NextSibling;
+        if (sibling != null)
+        {
+            return sibling;
+        }
+        return node.ParentNode;
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,8 +6,20 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private Script script;
 
-    void Start()
+    private bool scriptExecuted = false;
+
+    private void Start()
     {
         StartCoroutine(Director.Instance.LoadScript(script));
+    }
+
+    private void Update()
+    {
+        if (Director.Instance.ScriptLoaded && !scriptExecuted)
+        {
+            scriptExecuted = true;
+            Director.Instance.ReadScript();
+            Director.Instance.RunScript();
+        }
     }
 }
