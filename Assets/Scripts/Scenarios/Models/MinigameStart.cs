930ffba4d07f2ad4af583283f964031400c65d5d
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-01-25 15:07:06 +0100

    Add minigame model

---
+++ Assets/Scripts/Scenarios/Models/MinigameStart.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TeaBase { EarlGray, Mint, Coffee }
+public enum TeaSyroup { Mango, Strawberry, Milk }
+public enum TeaJelly { Chocholate, Almond, Banana }
+public enum TeaMixage { Light, Medium, Hard }
+
+public class MinigameStart
+{
+    public readonly TeaBase teaBase;
+    public readonly TeaSyroup teaSyroup;
+    public readonly TeaSyroup? secondSyroup;
+    public readonly List<TeaJelly> teaJellies;
+    public readonly TeaMixage teaMixage;
+
+    public MinigameStart(TeaBase teaBase, TeaSyroup teaSyroup, TeaSyroup? secondSyroup, List<TeaJelly> teaJellies, TeaMixage teaMixage)
+    {
+        this.teaBase = teaBase;
+        this.teaSyroup = teaSyroup;
+        this.secondSyroup = secondSyroup;
+        this.teaJellies = teaJellies;
+        this.teaMixage = teaMixage;
+    }
+}
