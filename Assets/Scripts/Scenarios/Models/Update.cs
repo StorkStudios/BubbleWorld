3c706939e7c59763cf98135d9b0740be8cc5180f
Pojemnik <andrzej.gauza@outlook.com>
2025-01-25 18:56:23 +0100

    Fix update parameters optionalness

--- Assets/Scripts/Scenarios/Models/Update.cs
+++ Assets/Scripts/Scenarios/Models/Update.cs
@@ -2,7 +2,44 @@
 using System.Xml.Serialization;
 
 [XmlRoot("Update")]
-public class Update : Enter
+public class Update : ChapterElement
 {
+    [XmlAttribute("character_id")]
+    public string characterId;
 
+    [XmlAttribute("eyes")]
+    public string eyesPose;
+
+    [XmlAttribute("mouth")]
+    public string mouthPose;
+
+    [XmlAttribute("position")]
+    public string positionValue;
+
+    [XmlIgnore]
+    public CharacterData.EyesPose? eyes
+    {
+        get
+        {
+            return eyesPose == null ? null :(CharacterData.EyesPose)System.Enum.Parse(typeof(CharacterData.EyesPose), eyesPose);
+        }
+    }
+
+    [XmlIgnore]
+    public CharacterData.MouthPose? mouth
+    {
+        get
+        {
+            return mouthPose == null ? null : (CharacterData.MouthPose)System.Enum.Parse(typeof(CharacterData.MouthPose), mouthPose);
+        }
+    }
+
+    [XmlIgnore]
+    public float? position
+    {
+        get
+        {
+            return positionValue == null ? null : float.Parse(positionValue);
+        }
+    }
 }
