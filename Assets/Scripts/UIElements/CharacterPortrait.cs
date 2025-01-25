e70c13aad6caa8c4c8937f3784cef7c0dba4fafa
Szymon Kaczmarek <szymon.kaczmarek111@gmail.com>
2025-01-25 18:19:56 +0100

    Add spawning characters

--- Assets/Scripts/UIElements/CharacterPortrait.cs
+++ Assets/Scripts/UIElements/CharacterPortrait.cs
@@ -1,15 +1,45 @@
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(CanvasGroup))]
 public class CharacterPortrait : MonoBehaviour
 {
+    [SerializeField]
+    private Image baseImage;
+    [SerializeField]
+    private Image eyesImage;
+    [SerializeField]
+    private Image mouthImage;
+
+    public Sprite Base
+    {
+        get => baseImage.sprite;
+        set => baseImage.sprite = value;
+    }
+
+    public Sprite Eyes
+    {
+        get => eyesImage.sprite;
+        set => eyesImage.sprite = value;
+    }
+
+    public Sprite Mouth
+    {
+        get => mouthImage.sprite;
+        set => mouthImage.sprite = value;
+    }
+
+    public CanvasGroup CanvasGroup => canvasGroup;
+
     public float position;
 
     private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
     
     private void Awake()
     {
         rectTransform = transform as RectTransform;
+        canvasGroup = GetComponent<CanvasGroup>();
     }
 
     private void Start()
--- Assets/Scripts/UIElements/CharacterPortraitsSection.cs
+++ Assets/Scripts/UIElements/CharacterPortraitsSection.cs
@@ -1,12 +1,43 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CharacterPortraitsSection : MonoBehaviour
 {
     [SerializeField]
+    private CharacterPortrait characterPortraitPrefab;
+    [SerializeField]
+    private float fadeDuration;
+    [SerializeField]
     private SerializedDictionary<string, CharacterData> characters;
 
-    public void SpawnCharacter(Enter enter)
+    private SerializedDictionary<string, CharacterPortrait> characterPortraits = new SerializedDictionary<string, CharacterPortrait>();
+
+    private void Start()
     {
-        
+        Director.Instance.ElementReadEvent += OnElementRead;
+    }
+
+    private void OnElementRead(string element, ChapterElement values)
+    {
+        switch (element)
+        {
+            case "Enter":
+                SpawnCharacter(values as Enter);
+                break;
+        }
+    }
+
+    private void SpawnCharacter(Enter values)
+    {
+        CharacterData characterData = characters[values.characterId];
+
+        CharacterPortrait characterPortrait = Instantiate(characterPortraitPrefab.gameObject, transform).GetComponent<CharacterPortrait>();
+        characterPortrait.position = values.position;
+        characterPortrait.Base = characterData.BaseSprite;
+        characterPortrait.Eyes = characterData.Eyes[values.eyes];
+        characterPortrait.Mouth = characterData.Mouth[values.mouth];
+
+        characterPortrait.CanvasGroup.alpha = 0;
+        characterPortrait.CanvasGroup.DOFade(1, fadeDuration);
     }
 }
